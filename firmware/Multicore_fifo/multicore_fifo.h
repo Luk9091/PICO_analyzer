d411b1a206612cb1ac1d5e91345c38000efc1446
Devraux <krzysztof.plonka2@onet.pl>
2025-05-14 20:56:30 +0200

    empty commit

--- firmware/Multicore_fifo/multicore_fifo.h
+++ firmware/Multicore_fifo/multicore_fifo.h
@@ -11,9 +11,6 @@
 #define mc_fifoBuffer2 2
 #define mc_fifoBuffer3 3
 
-#define digital_analyzerMaxBufferSize 256
-
-#define mc_readyCore1 0x00000001
 
 /// @note
 /// Pi Pico multicore fifo has 8 words each 32 bit's 
@@ -33,21 +30,6 @@
 
 
 
-/// @brief Core0 fifo helper function - Valid buffer setter
-/// @param buffer_number    - valid buffer number | 1 | 2 | 3 | 4 |
-/// @param valid_bufferSize - valid buffer size [0 - 255]
-/// @return true -> valid buffer was successfully set, false otherwise
-bool fifo_trySetValidBuffer(uint8_t buffer_number, uint8_t valid_bufferSize);
 
-/// @brief Core1 fifo helper function - Ready status register setter
-/// @param  - --
-/// @return true -> status was successfully set, false otherwise
-bool fifo_trySetReadStatus(void);
-
-/// @brief Core1 fifo helper function - Valid buffer getter
-/// @param buffer_number - --
-/// @param buffer_size - --
-/// @return true -> read buffer was successfully read, false otherwise
-bool fifo_tryReadBufferData(uint8_t *buffer_number, uint8_t *buffer_size);
 
 #endif
